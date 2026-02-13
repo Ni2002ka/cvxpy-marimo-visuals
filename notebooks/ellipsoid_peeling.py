d750dec9b350347536f3e33b79aa5134b08762c3
Ni2002ka <nika2002zahedi@gmail.com>
2026-02-13 11:38:35 -0800

    test

--- notebooks/ellipsoid_peeling.py
+++ notebooks/ellipsoid_peeling.py
@@ -51,75 +51,17 @@ def _(ChartPuck, cp, mo, np):
 
 
     def draw_func(ax, widget):
-        positions = np.array([[widget.x[i], widget.y[i]] for i in range(n)])
-        A = cp.Variable((2, 2), PSD=True)
-        b = cp.Variable(2)
-
-        cons = []
-        for j in range(n):
-            cons += [cp.norm2(A @ positions[j] + b) <= 1]
-
-        prob = cp.Problem(cp.Maximize(cp.log_det(A)), cons)
-        prob.solve(solver=cp.CLARABEL, max_iter=50)
-
-
-        Ahat = A.value
-        bhat = b.value
-
-        if Ahat is None or bhat is None:
-            raise ValueError("No values set for A or b")
-
-        # parameterize boundary
-        theta = np.linspace(0, 2*np.pi, 400)
-        U = np.vstack([np.cos(theta), np.sin(theta)])          # Pick points on unit circle
-        X = np.linalg.solve(Ahat, U - bhat.reshape(2,1))       # Find pre-image of points
-
-
-        ax.scatter(positions[:, 0], positions[:, 1], s=20, label="points")
-        ax.plot(X[0, :], X[1, :], linewidth=2, label="ellipsoid boundary")
-
-        # Duals calculation
-        lambdas = np.array([c.dual_value for c in cons], dtype=float)  # shape (n,)
-        lambdas = np.maximum(lambdas, 0.0)
-
-        Y = (Ahat @ positions.T + bhat.reshape(2, 1))
-        Ynorm = np.linalg.norm(Y, axis=0) + 1e-12
-        N = (Ahat.T @ (Y / Ynorm))
-
-        if np.max(lambdas) > 0:
-            lam_scaled = lambdas / np.max(lambdas)
-        else:
-            lam_scaled = lambdas
-
-        base = 2.0
-        Ux = -N[0, :] * (base * lam_scaled)
-        Uy = -N[1, :] * (base * lam_scaled)
-
-        ax.quiver(
-            positions[:, 0], positions[:, 1],
-            Ux, Uy,
-            angles="xy", scale_units="xy", scale=1,
-            width=0.006, alpha=0.9,
-            label="dual normal force (scaled)"
+        ax.set_title("hello")
+        multi_puck = ChartPuck.from_callback(
+            draw_fn=draw_func,
+            figsize=(8, 8),
+            x_bounds=(-4, 4),
+            y_bounds=(-4, 4),
+            x=[-1.5] + [0] * (n-2) + [1.5],
+            y=[0] + [1.5] * (n-1),
+            puck_color="red",
+            throttle=100
         )
-
-        ax.set_xlim(-4, 4)
-        ax.set_ylim(-4, 4)
-        ax.set_xlabel("X")
-        ax.set_ylabel("Y")
-        ax.set_title("Drag points to move them")
-        ax.grid(True, alpha=0.3)
-
-    multi_puck = ChartPuck.from_callback(
-        draw_fn=draw_func,
-        figsize=(8, 8),
-        x_bounds=(-4, 4),
-        y_bounds=(-4, 4),
-        x=[-1.5] + [0] * (n-2) + [1.5],
-        y=[0] + [1.5] * (n-1),
-        puck_color="red",
-        throttle=100
-    )
 
     multi_widget = mo.ui.anywidget(multi_puck)
     return (multi_widget,)
